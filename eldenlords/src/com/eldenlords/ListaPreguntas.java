71d2496086cf5993fb0494ff59167e28e61f9438
gabri <gabri@10.247.47.14>
2024-05-02 12:48:24 +0200

    si papi

--- eldenlords/src/com/eldenlords/ListaPreguntas.java
+++ eldenlords/src/com/eldenlords/ListaPreguntas.java
@@ -1,4 +1,12 @@
+<<<<<<< Upstream, based on elden_lords/master
 package com.eldenlords;
+=======
+<<<<<<< Upstream, based on elden_lords/master
+
+=======
+package com.eldenlords;
+>>>>>>> c1a5d49 si papi
+>>>>>>> 4c9838c si papi
 public class ListaPreguntas {
 	
 }
