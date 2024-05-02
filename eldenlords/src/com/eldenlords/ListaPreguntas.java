4d2ae84d686c4b4f6770c329466fa480f9491bad
gabri <gabri@LAPTOP-SUKM0I0A>
2024-05-02 14:38:30 +0200

    Jugador con atributos definidos, ListaPreguntas creada

--- eldenlords/src/com/eldenlords/ListaPreguntas.java
+++ eldenlords/src/com/eldenlords/ListaPreguntas.java
@@ -1,19 +1,6 @@
-<<<<<<< Upstream, based on elden_lords/master
-<<<<<<< Upstream, based on elden_lords/master
-package com.eldenlords;
-=======
-<<<<<<< Upstream, based on elden_lords/master
 
-=======
-=======
->>>>>>> e24187f x
 package com.eldenlords;
-<<<<<<< Upstream, based on elden_lords/master
->>>>>>> c1a5d49 si papi
->>>>>>> 4c9838c si papi
-=======
 
->>>>>>> e24187f x
 public class ListaPreguntas {
 	
 }
